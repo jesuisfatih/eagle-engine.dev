f027cb330b33bfdafda12d1677977d2f5ee69f32
jesuisfatih <jesuisfatih@users.noreply.github.com>
2025-11-25 17:53:44 +0300

    Fix EventsModule imports

--- backend/src/events/events.module.ts
+++ backend/src/events/events.module.ts
@@ -3,12 +3,14 @@ import { BullModule } from '@nestjs/bull';
 import { EventsService } from './events.service';
 import { EventsController } from './events.controller';
 import { EventsProcessorWorker } from './workers/events-processor.worker';
+import { ShopifyModule } from '../shopify/shopify.module';
 
 @Module({
   imports: [
     BullModule.registerQueue({
       name: 'events-raw-queue',
     }),
+    ShopifyModule,
   ],
   controllers: [EventsController],
   providers: [EventsService, EventsProcessorWorker],
