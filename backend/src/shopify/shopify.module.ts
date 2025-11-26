cb4c682da0980fd2650db920992758c012ca39f7
jesuisfatih <jesuisfatih@users.noreply.github.com>
2025-11-26 08:38:08 +0300

    FIX: ShopifyModule HttpModule import + Checkout function Multipass SSO

--- backend/src/shopify/shopify.module.ts
+++ backend/src/shopify/shopify.module.ts
@@ -1,22 +1,26 @@
 import { Module } from '@nestjs/common';
+import { HttpModule } from '@nestjs/axios';
 import { ShopifyGraphqlService } from './shopify-graphql.service';
 import { ShopifyCustomerSyncService } from './shopify-customer-sync.service';
 import { ShopifySsoService } from './shopify-sso.service';
+import { ShopifyService } from './shopify.service';
 import { PrismaService } from '../prisma/prisma.service';
-import { ConfigService } from '@nestjs/config';
+import { ConfigModule } from '@nestjs/config';
 
 @Module({
+  imports: [HttpModule, ConfigModule],
   providers: [
     ShopifyGraphqlService,
     ShopifyCustomerSyncService,
     ShopifySsoService,
+    ShopifyService,
     PrismaService,
-    ConfigService,
   ],
   exports: [
     ShopifyGraphqlService,
     ShopifyCustomerSyncService,
     ShopifySsoService,
+    ShopifyService,
   ],
 })
 export class ShopifyModule {}
